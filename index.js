c24e2c16c6e8a28899bb4fa3fa8ed1757641ce35
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-07-01 11:07:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 5;
+console.log(x + y);
+var z = 15.4984949;
+console.log(z);
+//estudando string
+var nome = "juan";
+console.log(nome.toLowerCase());
+//valores boleans
+var a = false;
+console.log(a);
+console.log(typeof a);
---
+++ index.ts
@@ -0,0 +1,37 @@
+let x: number = 10
+let y:number = 5
+
+console.log(x + y)
+
+
+const z:number = 15.4984949
+
+console.log(z)
+
+
+//estudando string
+ 
+let nome:string = "juan";
+
+console.log(nome.toLowerCase())
+
+
+//valores boleans
+
+let a: boolean = false
+
+console.log(a)
+
+console.log(typeof a)
+
+
+
+//programar em ts é como um pair programming
+
+//ele sempre vai falar oque está errado
+
+// inference é quando o typescript define o tipo de dados para nó
+
+
+let inf = "test"
+
